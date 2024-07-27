f3038aefa7b6e723c20fbac8ac3f43792d638442
youssef623 <youssefyasser623@gmail.com>
2024-07-27 09:31:50 +0300

    chart

--- lib/expenses.dart
+++ lib/expenses.dart
@@ -1,3 +1,4 @@
+import 'package:expenses_tracker/widgets/chart/chart.dart';
 import 'package:expenses_tracker/widgets/expenses_list/expenses_list.dart';
 import 'package:expenses_tracker/models/expense.dart';
 import 'package:expenses_tracker/widgets/expenses_list/new_expense.dart';
@@ -97,7 +98,7 @@ class _ExpensesState extends State<Expenses> {
       ),
       body: Column(
         children: [
-          const Text('The Chart'),
+          Chart(expenses: _registeredExpenses),
           Expanded(child: mainContent),
         ],
       ),
--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -11,20 +11,21 @@ enum Category {
   leisure,
   work,
 }
-const categoryIcons =
-{
-  Category.food : Icons.lunch_dining,
-  Category.travel : Icons.flight_takeoff,
-  Category.leisure : Icons.movie,
-  Category.work : Icons.work
+
+const categoryIcons = {
+  Category.food: Icons.lunch_dining,
+  Category.travel: Icons.flight_takeoff,
+  Category.leisure: Icons.movie,
+  Category.work: Icons.work
 };
+
 class Expense {
-  Expense({
-    required this.amount,
-    required this.title,
-    required this.date,
-    required this.category
-  }) : id = uuid.v4();
+  Expense(
+      {required this.amount,
+      required this.title,
+      required this.date,
+      required this.category})
+      : id = uuid.v4();
 
   final String id;
   final String title;
@@ -32,8 +33,29 @@ class Expense {
   final DateTime date;
   final Category category;
 
-  String get formattedDate
-  {
+  String get formattedDate {
     return formatter.format(date);
+  }
+}
+
+class ExpenseBucket {
+  const ExpenseBucket({
+    required this.category,
+    required this.expenses,
+  });
+  ExpenseBucket.forCategory(List<Expense> allExpenses, this.category)
+      : expenses = allExpenses
+            .where((expense) => expense.category == category)
+            .toList();
+
+  final Category category;
+  final List<Expense> expenses;
+
+  double get totalExpenses {
+    double sum = 0;
+    for (final Expense expense in expenses) {
+      sum += expense.amount;
+    }
+    return sum;
   }
 }
